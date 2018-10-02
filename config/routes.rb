e7f5a56974f50974b395bf99ec52c53377cb0eda
richard trapanese <richardtrapanese@gmail.com>
2018-10-02 14:10:48 -0400

    create a nav bar that goes to admin, teach and student pages. also added but commented out the logic for login logout

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :course
   resources :cohort
   resources :administrator
+  root 'welcom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
